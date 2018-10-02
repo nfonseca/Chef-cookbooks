92d99c54208e8fc9c7af81b07cd2922cd5775111
Nelson Fonseca <nelson.chamusca.fonseca@gmail.com>
2018-10-02 16:20:08 +0100

    Add files via upload
    new version

--- oracle12C/recipes/download-ora12c.rb
+++ oracle12C/recipes/download-ora12c.rb
@@ -6,11 +6,24 @@
 
 
 bash 'download_ora12c' do
+  live_stream true
   user 'root'
   cwd '/tmp'
   code <<-EOH
   wget http://192.168.10.153/Others/linuxx64_12201_database.zip
-  unzip linuxx64_12201_database.zip
+  unzip -o linuxx64_12201_database.zip
+  EOH
+end
+
+
+bash 'install-dev-libs' do
+  live_stream true 
+  user 'root'
+  code <<-EOH
+  yum install -y binutils.x86_64 compat-libcap1.x86_64 gcc.x86_64 gcc-c++.x86_64 glibc.i686 glibc.x86_64 \
+  glibc-devel.i686 glibc-devel.x86_64 ksh compat-libstdc++-33 libaio.i686 libaio.x86_64 libaio-devel.i686 libaio-devel.x86_64 \
+  libgcc.i686 libgcc.x86_64 libstdc++.i686 libstdc++.x86_64 libstdc++-devel.i686 libstdc++-devel.x86_64 libXi.i686 libXi.x86_64 \
+  libXtst.i686 libXtst.x86_64 make.x86_64 sysstat.x86_64
   EOH
 end
 
